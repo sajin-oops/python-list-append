9c9c925db8b0b48f9fa8fd4bf9596719e26d101f
A.K.SAJIN <108419506+sajin-oops@users.noreply.github.com>
2024-09-17 21:36:30 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,19 @@
+x = lambda a: a + 10
+print(x(5))
+
+# - O/P - 15
+
+l1=[1,2,3,4,5]
+l2=["Sajin","Aaron","Lijin"]
+print(l1)
+# - O/P = [1, 2, 3, 4, 5]
+print(l2)
+# O/P = ['Sajin', 'Aaron', 'Lijin']
+l1.append(7)
+l2.append("Nijas")
+print(l1)
+print(l2)
+'''
+[1, 2, 3, 4, 5, 7]
+['Sajin', 'Aaron', 'Lijin', 'Nijas']
+'''
